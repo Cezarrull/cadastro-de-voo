de677afaaa8700a35a77748ebf7ee667ee8563bc
Cezar <cezarrull12345@gmail.com>
2025-05-06 09:11:28 -0300

    :wrench: adjustments cpf

--- voo.py
+++ voo.py
@@ -3,29 +3,42 @@ cadastrosPessoa = dict()
 pessoas = 0
 
 while True:
-    print('\nEscolha uma das opções a baixo: ')
-    option = input('1 - cadastrar'
-    '\n2 - informações'
-    '\n\nopção: ')
+    print('\nEscolha uma das opções a baixo: \n'
+    '\n1 - cadastrar'
+    '\n2 - informações')
+    option = input('\n--> ')
     if option == '1':
-        numPessoas = int(input('Número de pessoas que deseja cadastra: '))
+        numPessoas = int(input('Número de pessoas que deseja cadastrar: '))
 
         while pessoas < numPessoas:
             print(f'\n{pessoas + 1}° Pessoa')
-            nome = input('Digite seu nome: ')
+            nome = input('Digite seu nome: ').title()
             cpf = input('Digite seu CPF: ')
+            while cpf.count('.') == 0 or cpf.count('-') == 0:
+                listCpf = []
+                cont = 0
+                for c in cpf:
+                    cont+=1
+                    listCpf.append(c)
+                    if cont == 3 or cont == 6:
+                        listCpf.append('.')
+                    elif cont == 9:
+                        listCpf.append('-')
+                break
+
             if cpf in cadastrosPessoa.keys():
                 print('\nCPF inválido, por favor digite o seu CPF!')
             else:
                 idade = input('Digite sua idade: ')
-                cidade = input('Digite sua CIDADE ORIGEM: ')
+                cidade = input('Digite sua cidade: ')
 
                 cadastrosPessoa[cpf] = {
                     'nome': nome,
                     'idade': idade,
-                    'cidade Origem': cidade
+                    'cidade origem': cidade
                 }
                 pessoas += 1
+            ##input('Para onde vão: ')
     elif option == '2':
         while True:
             optionInfo = input('\n1 - cadastros de clientes'
@@ -33,7 +46,13 @@ while True:
             '\n3 - sair'
             '\nopcão: ')
             if optionInfo == '1':
-                print(cadastrosPessoa)
+                for c, dados in cadastrosPessoa.items():
+                    print('\ncpf: ', end='')
+                    for i in listCpf:
+                        print(i, end='')
+                    print(f'\nnome: {dados['nome']}')
+                    print(f'idade: {dados['idade']}')
+                    print(f'cidade origem: {dados['cidade origem']}')
             elif optionInfo == '2':
                 print('voos**')
             elif optionInfo == '3':
